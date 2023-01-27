93bb3106aa8c8b4c0e0f64224ef6b78ed63d0c99
annakopeikina <anna.v.kopeikina@gmail.com>
2023-01-28 03:59:39 +0400

    001task added

--- 001task2_MinMax2numbers/Program.cs
+++ 001task2_MinMax2numbers/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿// Напишите программу, которая на входе принимает два числа и выдаёт, 
+//какое число больше, а какое меньше. 
+// a = 5; b = 7 --> max = 7
+// a = 2; b = 10 --> max =10
+// a = -9; b = -3 --> max = -3 
+Console.WriteLine("Введите первое число: ");
+string numberA = Console.ReadLine();
+Console.WriteLine("Введите второе число: ");
+string numberB = Console.ReadLine();
