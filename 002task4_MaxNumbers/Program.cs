9e48618e531e7f14a2f0775f421d6ab57c3caae6
annakopeikina <anna.v.kopeikina@gmail.com>
2023-01-28 16:41:55 +0400

    002 task has been drafted

---
+++ 002task4_MaxNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая принимает на вход 3 числа и
+//выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите первое число: ");
+int numberA =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberB =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int numberC =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Максимальное число: ");
+if(numberA > numberB > numberC)
+{
+    Console.WriteLine(numberA);
+}
+else if(numberB > numberC > numberA)
+{
+    Console.WriteLine(numberB);
+}
+else if(numberC > numberB > numberA)
+{
+    Console.WriteLine(numberC);
+}
+else 
+{
+    Console.WriteLine("Все числа равны");
+}
