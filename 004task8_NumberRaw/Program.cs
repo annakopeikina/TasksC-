51d2daf63e9a257b73d46774bfb86f0509eaa059
annakopeikina <anna.v.kopeikina@gmail.com>
2023-01-28 22:21:56 +0400

    Problems with nullable object have been fixed

--- 004task8_NumberRaw/Program.cs
+++ 004task8_NumberRaw/Program.cs
@@ -1,8 +1,7 @@
 ﻿//Напишите программу, которая на вход принимает число (N), а на выходе
 //показывает все четные числа от 1 до N.
 Console.WriteLine("Введите число N: ");
-string numberStr = Console.ReadLine();
-int number = Convert.ToInt32(numberStr);
+int number =Convert.ToInt32(Console.ReadLine());
 
 for (int u = number - number; u <= number; u=2 + u++)
 {
