f70b426b552ede41893fa9ce11b722ecf7580218
annakopeikina <anna.v.kopeikina@gmail.com>
2023-01-28 22:12:32 +0400

    004Task_NumberRaw has been created

---
+++ 004task8_NumberRaw/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе
+//показывает все четные числа от 1 до N.
+Console.WriteLine("Введите число N: ");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+
+for (int u = number - number; u <= number; u=2 + u++)
+{
+    Console.WriteLine($"{u} ");
+}
