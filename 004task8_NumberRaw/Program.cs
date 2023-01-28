f0e647a1566ed04954e7e51e5c8f64c5d7863800
annakopeikina <anna.v.kopeikina@gmail.com>
2023-01-28 22:29:22 +0400

    Fixing modifying

---
+++ 004task8_NumberRaw/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе
+//показывает все четные числа от 1 до N.
+Console.WriteLine("Введите число N: ");
+int number =Convert.ToInt32(Console.ReadLine());
+
+for (int u = number - number; u <= number; u=2 + u++)
+{
+    Console.WriteLine($"{u} ");
+}
