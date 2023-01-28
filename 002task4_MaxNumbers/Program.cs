a9f4dce129cec11dbba510bbd968fde098eb4090
annakopeikina <anna.v.kopeikina@gmail.com>
2023-01-28 19:24:00 +0400

    The Task of finding maximum number among 3

--- 002task4_MaxNumbers/Program.cs
+++ 002task4_MaxNumbers/Program.cs
@@ -7,19 +7,20 @@ int numberB =Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число: ");
 int numberC =Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Максимальное число: ");
-if(numberA > numberB > numberC)
+
+if((numberA > numberC) && (numberA > numberB))
 {
     Console.WriteLine(numberA);
 }
-else if(numberB > numberC > numberA)
+else if((numberB > numberA) && (numberB > numberC))
 {
     Console.WriteLine(numberB);
 }
-else if(numberC > numberB > numberA)
+else if((numberC > numberA) && (numberC > numberB))
 {
     Console.WriteLine(numberC);
 }
-else 
+else
 {
-    Console.WriteLine("Все числа равны");
+    Console.WriteLine("Максимального числа нет.Все три числа равны.");
 }
