232797804b0bee1a3fbbba50fd42c875c76947ab
annakopeikina <anna.v.kopeikina@gmail.com>
2023-01-28 20:38:12 +0400

    003Task_EvenNumber has been created

---
+++ 003task6_EvenNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число четным (делиться ли оно на 2 без остатка).
+Console.WriteLine("Введите число для проверки на четность: ");
+int numberA =Convert.ToInt32(Console.ReadLine());
+if (numberA % 2==0) 
+{
+    Console.WriteLine("Введенное число четное");
+}
+else if (numberA % 2 > 0)
+{
+    Console.WriteLine("Введенное число нечетное");
+}
+else if (numberA % 2 < 0)
+{
+    Console.WriteLine("Введенное число нечетное");
+}
