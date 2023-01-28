59d5d9d930fd1d320b0ab57ad69b1b9853de2725
annakopeikina <anna.v.kopeikina@gmail.com>
2023-01-28 15:26:45 +0400

    добавлена строка вывода результата

--- 001task2_MinMax2numbers/Program.cs
+++ 001task2_MinMax2numbers/Program.cs
@@ -7,6 +7,7 @@ Console.WriteLine("Введите первое число: ");
 int numberA =Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int numberB =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Максимальное число: ");
 if(numberA > numberB)
 {
     Console.WriteLine(numberA);
