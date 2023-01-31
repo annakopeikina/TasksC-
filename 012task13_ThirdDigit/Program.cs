02b3d3b51e4d3fa6c96cc8eb1177556a9233c7ee
annakopeikina <anna.v.kopeikina@gmail.com>
2023-01-31 22:48:23 +0400

    o12task13_ThirdDigit has been added

---
+++ 012task13_ThirdDigit/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number >= 1000) 
+{                                 
+   number = number / 10;  
+}    
+ 
+if ((number < 1000)&&(number > 100))
+{
+    Console.WriteLine(number % 10);
+}
+if (number < 100)
+{
+     Console.WriteLine("Третьей цифры нет.");
+}
