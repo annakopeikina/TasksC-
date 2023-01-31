330d4c2186e2c123655cf5fc903cccc3c5665c10
annakopeikina <anna.v.kopeikina@gmail.com>
2023-01-31 23:53:08 +0400

    013task15_DaysOff has been added

---
+++ 013task15_DaysOff/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход цифру
+//обозначающую день недели и проверяет, является ли этот день
+//Выходным.
+Console.WriteLine("Введите цифру дня недели (считая понедельник первым): ");
+string number = Console.ReadLine();
+if (number == "6")
+   Console.WriteLine("Выходной день");
+   else if (number == "7")
+   Console.WriteLine("Выходной день");
+else if (number == "1")
+   Console.WriteLine("Будний день");
+   else if (number == "2")
+   Console.WriteLine("Будний день");
+   else if (number == "3")
+   Console.WriteLine("Будний день");
+   else if (number == "4")
+   Console.WriteLine("Будний день");
+   else if (number == "5")
+   Console.WriteLine("Будний день");
+else Console.WriteLine("Вы ввели не день недели");
+
+                    
