4d4cc6c9a1a9f642ad7a6dda1dcf05d34ab04c21
annakopeikina <anna.v.kopeikina@gmail.com>
2023-02-03 00:50:16 +0400

    111task_Palindrom has been added,correction neded

---
+++ 111task19_Palindrom/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на ввод натуральное число и
+//проверяет, является ли оно палиндромом.
+//14212->нет
+//12821->да
+//23432->да
+
+Console.WriteLine("Введите натуральное пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int numberFirst = number / 10000 % 10;
+int numberSecond = number / 1000 % 10;
+int reverseBeforeLast = number / 10 % 10;
+int reverseLast = number % 10;
+      if (numberFirst == reverseLast && numberSecond == reverseBeforeLast)
+      {
+        Console.WriteLine("Введенное число - палиндром");
+      }
+      else
+      {
+        Console.WriteLine($"Введенное число - не палиндром");
+      } 
+    
