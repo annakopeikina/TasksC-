1d995a2ddac892957ff2a23113e19871cda77dc2
annakopeikina <anna.v.kopeikina@gmail.com>
2023-02-04 22:27:46 +0400

    Table of cubes from 1 to N has been added

---
+++ 113task23_CubeRawNumbers/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая принимает на вход число (N) и 
+//выдаёт таблицу кубов чисел от 1 до N.(Math.Pow(3))
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int cube = (number - number) + 1; cube <= number; cube++)
+{
+    Console.WriteLine(Math.Pow(cube,3));
+}
