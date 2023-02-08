0a6127aedf9fa87f7fb2545630f84f860b1b0579
annakopeikina <anna.v.kopeikina@gmail.com>
2023-02-08 22:05:50 +0400

    023task29_ArrayEnter has been added

---
+++ 023task29_ArrayEnter/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и
+//выводит их на экран.
+int []array = new int[8];
+Random rnd = new Random();
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(0,9);
+    Console.Write($"{array[i]},");
+}
+Console.Write("]");
