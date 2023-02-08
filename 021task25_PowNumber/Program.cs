5413378e2c7cbefc5b16d7f3ef3b68abb7e6d6f4
annakopeikina <anna.v.kopeikina@gmail.com>
2023-02-08 19:16:27 +0400

    021task25_PowNumber has been added

---
+++ 021task25_PowNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и 
+//возводит число А в натуральную степень B.
+//3,5->243(3^5)
+//2,4 = 16
+Console.WriteLine("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите натуральное число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberB > 0)
+{
+    Console.WriteLine(Math.Pow(numberA,numberB));
+}
+else
+{
+    Console.WriteLine("Неправильный ввод");
+}
