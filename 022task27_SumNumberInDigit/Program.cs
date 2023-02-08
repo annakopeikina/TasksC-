ba3c82653099c83c0f2374f64a09b9d02be003c7
annakopeikina <anna.v.kopeikina@gmail.com>
2023-02-08 20:23:16 +0400

    022task27_SumNumbersinDigit has been added

---
+++ 022task27_SumNumberInDigit/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход число и 
+//выдает сумму цифр в числе.
+
+Console.WriteLine("Введите число: ");
+var number = Convert.ToInt32(Console.ReadLine());
+var sum = 0;
+while(number > 0)
+{
+    sum += number % 10;
+    number /= 10;
+}
+Console.WriteLine(sum);
+
