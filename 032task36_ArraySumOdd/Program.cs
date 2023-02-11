74b6345cfcf517671b3876728835dd04a83cea69
annakopeikina <anna.v.kopeikina@gmail.com>
2023-02-12 01:17:45 +0400

    032task36_ArraySumOdd has been added

---
+++ 032task36_ArraySumOdd/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задайте одномерный массив, заполненный случайными числами.
+//Найдите сумму элементов, стоящих на нечётных позициях.
+Console.WriteLine("Задайте длину массива: ");
+int Length = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[Length];
+Random random = new Random();
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(-100, 100);
+    Console.Write($"{array[i]},");
+}
+Console.Write("]");
+
+int SumOddNumbers()
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i += 2)
+    {
+
+        sum += array[i];
+
+    }
+    return sum;
+}
+Console.WriteLine(SumOddNumbers());
