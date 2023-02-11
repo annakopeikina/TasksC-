67fb310d20d0fcc5d8f6eb125a0037415ed6df21
annakopeikina <anna.v.kopeikina@gmail.com>
2023-02-11 23:36:27 +0400

    031task34_ArrayRandom3digitEven has been added

---
+++ 031task34_ArrayRandom3digitEven/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задайте массив, заполненный случайными положительными трёхзначными числами,
+//Напишите программу, которая покажет количество четных чисел в массиве.
+//[345, 897, 568, 234] ->2
+Console.WriteLine("Задайте длину массива: ");
+int Length = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int [Length];
+Random random = new Random();
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(100,1000);
+    Console.Write($"{array[i]}, ");
+}
+Console.Write("]"); 
+
+int GetNumberofEven()
+{
+    int number = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0)
+        number = number + 1;
+    }
+return number;
+}
+Console.WriteLine($" -> {GetNumberofEven()}");
+
