11d220335effbc6731b38c32e7b88687120c432e
annakopeikina <anna.v.kopeikina@gmail.com>
2023-02-12 00:02:56 +0400

    031task34_ArrayRandom3digitEven has been formated

--- 031task34_ArrayRandom3digitEven/Program.cs
+++ 031task34_ArrayRandom3digitEven/Program.cs
@@ -4,25 +4,25 @@
 Console.WriteLine("Задайте длину массива: ");
 int Length = Convert.ToInt32(Console.ReadLine());
 
-int[] array = new int [Length];
+int[] array = new int[Length];
 Random random = new Random();
 Console.Write("[");
 for (int i = 0; i < array.Length; i++)
 {
-    array[i] = random.Next(100,1000);
+    array[i] = random.Next(100, 1000);
     Console.Write($"{array[i]}, ");
 }
-Console.Write("]"); 
+Console.Write("]");
 
 int GetNumberofEven()
 {
     int number = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if(array[i] % 2 == 0)
-        number = number + 1;
+        if (array[i] % 2 == 0)
+            number = number + 1;
     }
-return number;
+    return number;
 }
 Console.WriteLine($" -> {GetNumberofEven()}");
 
