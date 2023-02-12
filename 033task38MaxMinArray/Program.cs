0ab4e6f0bfc8b0e0cbff62b1b066262da67f013f
annakopeikina <anna.v.kopeikina@gmail.com>
2023-02-12 05:50:24 +0400

    033task38_DifMaxMinArray has been added

---
+++ 033task38MaxMinArray/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задайте массив вещественных чисел. Найдите разницу между 
+//максимальным и минимальным элементом массива.
+Console.WriteLine("Задайте длину массива: ");
+int Length = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[Length];
+Random random = new Random();
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(-60, 30);
+    Console.Write($"{array[i]}, ");
+}
+Console.Write("]");
+double FindMax()
+{
+    double max = 0;
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > max)
+                max = array[i];
+        }
+        return max;
+    }
+}
+double FindMin()
+{
+    double min = array.Min();
+
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] < min)
+                min = array[i];
+        }
+        return min;
+
+    }
+}
+Console.Write(FindMax() - FindMin());
