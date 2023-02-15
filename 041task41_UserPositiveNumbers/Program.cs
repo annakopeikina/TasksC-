b3b2286173dfb043f6182468cf1b45c39ae5d87a
annakopeikina <anna.v.kopeikina@gmail.com>
2023-02-15 23:08:15 +0400

    041task41UserPositiveNumbers created warningCS8602

---
+++ 041task41_UserPositiveNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿//Пользователь вводит с клавиатуры М чисел. Посчитайте,
+//сколько чисел больше 0 ввел пользователь.
+//0,7,8,-2,-2,->2
+//1,7,567,89,223->3
+Console.WriteLine("Введите через пробел несколько целых чисел: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+for (int i = 0; i < array.Length; i++) 
+    {
+        if (array[i] > 0)
+        {
+           count++;
+        }
+    }        
+Console.WriteLine($"{count} чисел больше нуля");
+
+
+
+
