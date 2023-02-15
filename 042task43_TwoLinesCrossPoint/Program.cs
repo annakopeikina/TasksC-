a75589081cd655099dacf104f4229d9df403f041
annakopeikina <anna.v.kopeikina@gmail.com>
2023-02-16 01:27:14 +0400

    042task43_TwoLinesCrossPoint corrected with double

--- 042task43_TwoLinesCrossPoint/Program.cs
+++ 042task43_TwoLinesCrossPoint/Program.cs
@@ -3,14 +3,14 @@
 значения b1; k1; b2 и k2 задаются пользователем.
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 0,15)
 */
-Console.WriteLine("Введите значение параметра k1 координаты первой прямой: ");
-int k1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите значение параметра b1 координаты первой прямой: ");
-int b1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите значение параметра k2 координаты второй прямой: ");
-int k2 = Convert.ToInt32(Console.ReadLine());
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение параметра k1 координаты первой прямой: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите значение параметра b2 координаты второй прямой: ");
-int b2 = Convert.ToInt32(Console.ReadLine());
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение параметра k2 координаты второй прямой: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
 //x=(y-b1)/k1; y=(b2k1-b1k2)/k1-k2;
 //x*k1=y-b1; x*k1=((b2*k1-b1*k2)/(k1-k2))-b1;
 //(x*k1)*(k1-k2)=b2*K1-b1*k1
