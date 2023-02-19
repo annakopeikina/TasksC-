e3894d2aece7309f56a576de073f27a72145f774
annakopeikina <anna.v.kopeikina@gmail.com>
2023-02-19 23:19:24 +0400

    051task47_2DArrayDouble has to bee masked to straight the colomns or get another count function to spare space of 4.

--- 051task47_2DArrayDouble/Program.cs
+++ 051task47_2DArrayDouble/Program.cs
@@ -29,10 +29,10 @@ void PrintArray(double[,] array)
         
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write(array[i,j]+ "   ");
+            Console.Write($"{array[i,j]}  |  ");
         }
         
-        Console.WriteLine(" ");
+        Console.WriteLine("");
     }
 }
     
