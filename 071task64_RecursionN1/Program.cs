3e06f283852f907c3d53945b347510561725463b
annakopeikina <anna.v.kopeikina@gmail.com>
2023-02-26 01:39:34 +0400

    071task64_RecursionN1 if negative number correction with breakes is needed

---
+++ 071task64_RecursionN1/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задайте значение N. Напишите программу,
+которая выведет все натуральные числа в промежутке от N до 1.
+Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+*/
+Console.WriteLine("Введите натуральное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int M = 1;
+if (N < 1)
+{
+    Console.WriteLine("Некорректный ввод");
+    
+}
+
+
+int Numbers(int N, int M)
+{
+    if (N == M)
+    {
+    return N;
+    }
+    else
+    {
+     Console.Write($"{Numbers(N, M+1)}, ");
+     return M;
+    }
+}
+Console.WriteLine(Numbers(N,M));
+
