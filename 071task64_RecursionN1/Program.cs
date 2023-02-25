2d4ba60ac2ea5dfed78328adf6f2ba5c712455aa
annakopeikina <anna.v.kopeikina@gmail.com>
2023-02-26 02:38:00 +0400

    071task64_RecursionN1 has been corrected formated

--- 071task64_RecursionN1/Program.cs
+++ 071task64_RecursionN1/Program.cs
@@ -6,26 +6,37 @@ N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
 */
 Console.WriteLine("Введите натуральное число: ");
-int N = Convert.ToInt32(Console.ReadLine());
 int M = 1;
-if (N < 1)
-{
-    Console.WriteLine("Некорректный ввод");
-    
-}
+int N = GetNumber("N");
+Console.WriteLine(Numbers(N, M));
 
+int GetNumber(string message)
+{
+    int result = 0;
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод. Повторите ввод");
+        }
+    }
+    return result;
+}
 
 int Numbers(int N, int M)
 {
     if (N == M)
     {
-    return N;
+        return N;
     }
     else
     {
-     Console.Write($"{Numbers(N, M+1)}, ");
-     return M;
+        Console.Write($"{Numbers(N, M + 1)}, ");
+        return M;
     }
 }
-Console.WriteLine(Numbers(N,M));
-
