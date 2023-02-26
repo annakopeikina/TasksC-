7d506468d2e7a917f55b4f57f99d4ddeda778edc
annakopeikina <anna.v.kopeikina@gmail.com>
2023-02-26 05:35:43 +0400

    073task68_AkkermansRecursion has been formated

--- 073task68_AkkermansRecursion/Program.cs
+++ 073task68_AkkermansRecursion/Program.cs
@@ -12,10 +12,10 @@ Console.WriteLine($"-> A {n}, {m} = {Akkerman(n, m)}");
 int Akkerman(int n, int m)
 {
     if (n == 0)
-        return m+1;
+        return m + 1;
     else
       if (n != 0 && m == 0)
-        return Akkerman(n -1, 1);
-    else 
-        return Akkerman(n -1, Akkerman(n, m -1));
+        return Akkerman(n - 1, 1);
+    else
+        return Akkerman(n - 1, Akkerman(n, m - 1));
 }
